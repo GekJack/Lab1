9625ab26b411861482b65508e34190795e4bdf3b
GekJack <mma180904@gmail.com>
2025-04-10 17:56:25 +0300

    Creating sign-up module

---
+++ Scripts/controller/reg_controller.js
@@ -0,0 +1,40 @@
+export default class FormController {
+    constructor(model, view) {
+      this.model = model;
+      this.view = view;
+    }
+  
+    initialize() {
+        const confirmButton = document.getElementById("RegConBut");
+        const form = document.getElementById("RegForm");
+  
+      confirmButton.addEventListener("click", (event) => {
+        event.preventDefault();
+        this.saveDataToLocalStorage();
+      });
+  
+      form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        this.saveDataToLocalStorage();
+      });
+    }
+  
+    saveDataToLocalStorage() {
+      const userData = this.view.getUserData();
+  
+      if (!this.model.validateUserData(userData)) {
+        return;
+      }
+  
+      if (!this.model.checkExistingAccount(userData)) {
+        return;
+      }
+  
+      this.model.saveUserData(userData);
+      this.view.showSuccessMessage();
+      this.view.resetForm();
+      this.model.setCurrentAccount(userData);
+      window.location.href = "./index.html";
+    }
+  }
+  
---
+++ Scripts/view/reg_view.js
@@ -0,0 +1,19 @@
+export default class FormView {
+    getUserData() {
+      return {
+        name: document.getElementById("inputName").value,
+        password: document.getElementById("inputPassword").value,
+        email: document.getElementById("inputEmail").value,
+        dob: document.getElementById("inputDOB").value,
+      };
+    }
+  
+    showSuccessMessage() {
+      alert("Дані успішно збережено!");
+    }
+  
+    resetForm() {
+      document.querySelector("form").reset();
+    }
+  }
+  
