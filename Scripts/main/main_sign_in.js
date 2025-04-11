221d6ebc0be4f86f05b77352232185e8455708c3
GekJack <mma180904@gmail.com>
2025-04-11 17:11:16 +0300

    ...

--- Scripts/main/main_sign_in.js
+++ Scripts/main/main_sign_in.js
@@ -2,6 +2,7 @@ import LoginModel from "/Lab1/Scripts/models/sign_in_model.js";
 import LoginView from "/Lab1/Scripts/view/sign_in_view.js";
 import LoginController from "/Lab1/Scripts/controller/sign_in_controller.js";
 
+
 document.addEventListener("DOMContentLoaded", function () {
   const loginModel = new LoginModel();
   const loginView = new LoginView();
