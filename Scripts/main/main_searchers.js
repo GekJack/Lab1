2c6f2ed2a08b4dc70c80d76ecc6e7ad54b693248
GekJack <mma180904@gmail.com>
2025-04-11 19:07:55 +0300

    ...

--- Scripts/main/main_searchers.js
+++ Scripts/main/main_searchers.js
@@ -1,6 +1,6 @@
-import PostModel from "Lab1/Scripts/models/searchers_model.js";
-import PostView from "Lab1/Scripts/view/searchers_view.js";
-import PostController from "Lab1/Scripts/controller/searchers_controller.js";
+import PostModel from "/Lab1/Scripts/models/searchers_model.js";
+import PostView from "/Lab1/Scripts/view/searchers_view.js";
+import PostController from "/Lab1/Scripts/controller/searchers_controller.js";
 
 document.addEventListener("DOMContentLoaded", function () {
   const model = new PostModel();
