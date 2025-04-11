2d09aa9b5b0b552e98c4b86a88f825b1b6b49be0
GekJack <mma180904@gmail.com>
2025-04-11 22:30:18 +0300

    ...

--- Scripts/main/main_searchers.js
+++ Scripts/main/main_searchers.js
@@ -2,8 +2,10 @@ import PostModel from "/Lab1/Scripts/models/searchers_model.js";
 import PostView from "/Lab1/Scripts/view/searchers_view.js";
 import PostController from "/Lab1/Scripts/controller/searchers_controller.js";
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
   const model = new PostModel();
   const view = new PostView();
   const controller = new PostController(model, view);
 });
+
+
--- Scripts/view/searchers_view.js
+++ Scripts/view/searchers_view.js
@@ -1,64 +1,54 @@
 export default class PostView {
-    constructor() {
-      this.modal = document.getElementById("postModal");
-      this.openBtn = document.getElementById("addPostBtn");
-      this.closeBtn = document.getElementById("closeModal");
-      this.submitBtn = document.getElementById("submitPost");
-      this.titleInput = document.getElementById("postTitle");
-      this.contentInput = document.getElementById("postContent");
-      this.postsContainer = document.getElementById("postsContainer");
-    }
-  
-    bindEvents(onSubmit, getUsername) {
-      this.openBtn.onclick = () => {
-        const username = getUsername();
-        if (!username) {
-          alert("Щоб написати пост, увійдіть у свій акаунт!");
-          return;
-        }
-        this.modal.style.display = "block";
-      };
-  
-      this.closeBtn.onclick = () => {
-        this.modal.style.display = "none";
-      };
-  
-      window.onclick = (event) => {
-        if (event.target === this.modal) {
-          this.modal.style.display = "none";
-        }
-      };
-  
-      this.submitBtn.onclick = () => {
-        const title = this.titleInput.value.trim();
-        const content = this.contentInput.value.trim();
-        if (title && content) {
-          onSubmit(title, content);
-          this.clearForm();
-          this.modal.style.display = "none";
-        }
-      };
-    }
-  
-    clearForm() {
-      this.titleInput.value = "";
-      this.contentInput.value = "";
-    }
-  
-    renderPost(post) {
-      const div = document.createElement("div");
-      div.className = "discussion-card";
-      div.innerHTML = `
-        <div class="discussion-header">
-          <img src="https://www.w3schools.com/w3images/avatar4.png" alt="User Avatar">
-          <div class="author">${post.username}</div>
-          <div class="time">${post.time}</div>
-        </div>
-        <h4>${post.title}</h4>
-        <p>${post.content}</p>
-        <div class="discussion-footer">Буду радий вашому відгуку!</div>
-      `;
-      this.postsContainer.prepend(div);
-    }
+  constructor() {
+    this.modalEl = document.getElementById("postModal");
+    this.modal = new bootstrap.Modal(this.modalEl); 
+
+    this.openBtn = document.getElementById("addPostBtn");
+    this.submitBtn = document.getElementById("submitPost");
+    this.titleInput = document.getElementById("postTitle");
+    this.contentInput = document.getElementById("postContent");
+    this.postsContainer = document.getElementById("postsContainer");
   }
-  
+
+  bindEvents(onSubmit, getUsername) {
+    this.openBtn.onclick = () => {
+      const username = getUsername();
+      if (!username) {
+        alert("Щоб написати пост, увійдіть у свій акаунт!");
+        return;
+      }
+      this.modal.show(); 
+    };
+
+    this.submitBtn.onclick = () => {
+      const title = this.titleInput.value.trim();
+      const content = this.contentInput.value.trim();
+      if (title && content) {
+        onSubmit(title, content);
+        this.clearForm();
+        this.modal.hide();
+      }
+    };
+  }
+
+  clearForm() {
+    this.titleInput.value = "";
+    this.contentInput.value = "";
+  }
+
+  renderPost(post) {
+    const div = document.createElement("div");
+    div.className = "discussion-card";
+    div.innerHTML = `
+      <div class="discussion-header">
+        <img src="https://www.w3schools.com/w3images/avatar4.png" alt="User Avatar">
+        <div class="author">${post.username}</div>
+        <div class="time">${post.time}</div>
+      </div>
+      <h4>${post.title}</h4>
+      <p>${post.content}</p>
+      <div class="discussion-footer">Буду радий вам!</div>
+    `;
+    this.postsContainer.prepend(div);
+  }
+}
